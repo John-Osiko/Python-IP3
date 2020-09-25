11c9e1d4b2bcdb8020eb8e8118fa77b1a1a50fba
John-Osiko <johnmaxosiko@gmail.com>
2020-09-25 13:51:15 +0300

    Created decorators in file

---
+++ app/main/views.py
@@ -0,0 +1,104 @@
+from flask import render_template, redirect, url_for
+from flask.logging import login_required, current_user
+
+from . import main
+from .forms import PostForm, CommentForm, LoginProfile
+from ..models import Post, Comment, User, Upvote, Downvote
+
+
+@main.route('/')
+def index():
+    posts = Post.query.all()
+    product = Post.query.filter_by(category='product').all()
+    idea = Post.query.filter_by(category='idea').all()
+    business = Post.query.filter_by(category='Business').all()
+    return render_template('index.html', business=business, product=product, idea=idea, posts=posts)
+
+
+@main.route('/posts')
+@login_required
+def posts():
+    posts = Post.query.all()
+    likes = Upvote.query.all()
+    user = current_user
+    return render_template('global_pitches.html', posts=posts, likes=likes, user=user)
+
+
+@main.route('/new_post', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user._get_current_object().id
+        post_obj = Post(post=post, title=title, category=category, user_id=user_id)
+        post_obj.save()
+        return redirect(url_for('main.index'))
+    return render_template('pitches.html', form=form)
+
+
+@main.route('/comment/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def comment(post_id):
+    form = CommentForm()
+    post = Post.query.get(post_id)
+    user = User.query.all()
+    comments = Comment.query.filter_by(post_id=post_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        post_id = post_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(
+            comment=comment,
+            post_id=post_id,
+            user_id=user_id
+        )
+        new_comment.save()
+        new_comments = [new_comment]
+        print(new_comments)
+        return redirect(url_for('.comment', post_id=post_id))
+    return render_template('comments.html', form=form, post=post, comments=comments, user=user)
+
+
+@main.route('/user')
+@login_required
+def user():
+    username = current_user.username
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        return ('not found')
+    return render_template('user_profile.html', user=user)
+
+
+@main.route('/user/<name>/login_profile', methods=['POST', 'GET'])
+@login_required
+def loginprofile(name):
+    form = LoginProfile()
+    user = User.query.filter_by(username=name).first()
+    if user is None:
+        error = 'The user does not exist'
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save()
+        return redirect(url_for('.profile', name=name))
+    return render_template('profile/login_profile.html', form=form)
+
+
+@main.route('/like/<int:id>', methods=['POST', 'GET'])
+@login_required
+def upvote(id):
+    post = Post.query.get(id)
+    new_user_upvote = Upvote(post=post, upvote=1)
+    new_user_upvote.save()
+    return redirect(url_for('main.posts'))
+
+
+@main.route('/dislike/<int:id>', methods=['GET', 'POST'])
+@login_required
+def downvote(id):
+    post = Post.query.get(id)
+    vm = Downvote(post=post, downvote=1)
+    vm.save()
+    return redirect(url_for('main.posts'))
